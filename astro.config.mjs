bb781002453088f7f1363d5743da3881b2366cd9
drerrath <reonexroy12@gmail.com>
2025-11-14 23:24:26 +0530

    fix base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://drerrath.github.io",
-  base: "/vanitas-university",
+  base: "/vanita-university",
   vite: {
     plugins: [tailwindcss()],
   },
