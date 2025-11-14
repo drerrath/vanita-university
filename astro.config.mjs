c52cbef80265240d1929a600925501d4d3fd45f0
drerrath <reonexroy12@gmail.com>
2025-11-14 21:26:30 +0530

    add github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://drerrath.github.io",
+  base: "/vanitas-university",
   vite: {
     plugins: [tailwindcss()],
   },
